fd6acb3c4fcab192514220c0892f5210818fe45d
sidiqY26 <yusufikmi2021@gmail.com>
2023-11-13 11:02:56 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,148 @@
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+from pymongo import MongoClient
+import requests
+from datetime import datetime
+from bson import ObjectId
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    words_result = db.words.find({}, {'_id': False})
+    words = []
+    for word in words_result:
+        definition = word['definitions'][0]['shortdef']
+        definition = definition if type(definition) is str else definition[0]
+        words.append({
+            'word': word['word'],
+            'definition': definition,
+        })
+    msg = request.args.get('msg')
+    return render_template(
+        'index.html',
+        words=words,
+        msg=msg
+    )
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    api_key = 'c9a31243-09ee-4c50-9ff0-00442d85b48b'
+    url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{keyword}?key={api_key}'
+    response = requests.get(url)
+    definitions = response.json()
+    if not definitions:
+        return redirect(url_for('error', error_message=f'Could not find dictionary: "{keyword}"'))
+
+        # return redirect(url_for(
+        #     'error_word',
+        #     keyword=keyword
+        # ))
+    if type(definitions[0]) is str :
+        suggestions = ', '.join(definitions)
+        # return #redirect(url_for(
+        return render_template(
+            'error.html',
+            error_message= f'Could not find dictionary: "{keyword}", did you mean one of these words:"{suggestions}"'
+        )
+    
+    status=request.args.get('status_give', 'new')
+    return render_template(
+        'detail.html',
+        word=keyword,
+        definitions=definitions,
+        status=status
+    )
+
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    json_data = request.get_json()
+    word = json_data.get('word_give')
+    definitions = json_data.get('definitions_give')
+    doc = {
+        'word': word,
+        'date': datetime.now().strftime('%Y-%m-%d'),
+        'definitions': definitions,
+    }
+    db.words.insert_one(doc)
+    return jsonify({
+        'result': 'success',
+        'msg': f'the word: {word}, was saved!!!',
+    })
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    word = request.form.get('word_give')
+    db.words.delete_one({'word': word})
+    db.examples.delete_one({'word': word})
+    return jsonify({
+        'result': 'success',
+        'msg': f'the word: {word} was deleted'
+    })
+
+@app.route('/error')
+def error():
+    error_message = request.args.get('error_message', 'An error occurred.')
+    suggestions = request.args.get('suggestions', '')
+
+    return render_template('error.html', error_message=error_message, suggestions=suggestions)
+
+@app.route('/api/get_exs', methods=['GET'])
+def get_exs():
+    word = request.args.get('word')
+    example_data = db.examples.find({'word': word})
+    examples = []
+    for example in example_data:
+        examples.append({
+            'example': example.get('example'),
+            'id': str(example.get('_id')),
+        })
+    return jsonify({
+        "result": "success",
+        "examples": examples
+        })
+
+@app.route('/api/save_ex', methods=['POST'])
+def save_ex():
+    word = request.form.get('word')
+    example = request.form.get('example')
+    doc = {
+        'word': word,
+        'example': example,
+    }
+    db.examples.insert_one(doc)
+    return jsonify({
+        "result": "success",
+        'msg': f'Your example: {example}, for the word: {word}, was saved!',
+    })
+
+
+@app.route('/api/delete_ex', methods=['POST'])
+def delete_ex():
+    id = request.form.get("id")
+    word = request.form.get("word")
+    db.examples.delete_one({'_id': ObjectId(id)})
+    return jsonify(
+        {
+            "result": "success",
+            "msg": f"Youre example for the word: {word}, was deleted!"
+        })
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
